27670086d3ada9f4a26cfc92b66e9b1885d1f3b7
Drew <jordan.938@osu.edu>
2019-11-03 21:46:36 -0600

    Merge with package commit from previous work.

--- classes/PitchDeck.js
+++ classes/PitchDeck.js
@@ -9,19 +9,25 @@ The rules and uses of this class can be found in the repo's README
 
 
 class PitchDeck {
-    constructor(channel = 'pitch-deck-temp'){
-        this.channel = channel;
-    }
+  constructor(channel = 'pitch-deck-temp'){
+    this.channel = channel;
+  }
 
-    //region Methods
+  //region Main
 
-    /* 
-    Runs a game of Pitch Deck from start to finish.
+  /* 
+  Runs a game of Pitch Deck from start to finish.
 
-    Pulls game data from Drive, and is responsible for calling helper functions
-    below to follow flow of the game.  
-    */
-    game()
+  Pulls game data from Drive, and is responsible for calling helper functions
+  below to follow flow of the game.  
+  */
+  game()
 
-    //endregion
+  //endregion
+
+  //region Helpers
+
+  //TODO
+
+  //endregion
 }
